51daff89f40ab94e6c6b003acf5ed84c24318417
MoeTerani <moe.terani@gmail.com>
2019-08-27 12:20:29 +0200

    Navbar added and styled

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './header.style.scss';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <Link to='/' className='logo-container'>
+        <Logo className='logo'></Logo>
+      </Link>
+      <div className='options'>
+        <Link to='/shop' className='option'>
+          SHOP
+        </Link>
+
+        <Link to='/contact' className='option'>
+          CONATCT
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
